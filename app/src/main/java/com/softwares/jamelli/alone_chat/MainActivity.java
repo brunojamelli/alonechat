f571fe132f72f138b71ff2ed3027a269dd813363
brunogeek9 <brunogeek9@gmail.com>
2018-11-06 20:34:46 -0300

    adicionando menu ao projeto

--- app/src/main/java/com/softwares/jamelli/alone_chat/MainActivity.java
+++ app/src/main/java/com/softwares/jamelli/alone_chat/MainActivity.java
@@ -7,30 +7,27 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
     TabLayout tabLayout;
+    ViewPager vp;
+    PagerAdapter pa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initTabLayout();
 
-        ViewPager vp = findViewById(R.id.pager);
-        PagerAdapter pa = new ChatAdapter(getSupportFragmentManager());
-        vp.setAdapter(pa);
-        tabLayout = findViewById(R.id.tab);
-        tabLayout.setupWithViewPager(vp);
-
-        tabLayout.getTabAt(0).setIcon(R.drawable.camera2);
-     //   tabLayout.getTabAt(1).setIcon(R.drawable.chat);
-     //   tabLayout.getTabAt(2).setIcon(R.drawable.maps);
         vp.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
             @Override
             public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                switch (position){
+                switch (position) {
                     case 0:
-                        //tabLayout.getTabAt(0).setIcon(R.drawable.camera);
+                        //tabLayout.getTabAt(0);
                     case 1:
                         //tabLayout.getTabAt(1).setIcon(R.drawable.chat);
                     case 2:
@@ -52,4 +49,25 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void initTabLayout(){
+        vp = findViewById(R.id.pager);
+        pa = new ChatAdapter(getSupportFragmentManager());
+        vp.setAdapter(pa);
+        tabLayout = findViewById(R.id.tab);
+        tabLayout.setupWithViewPager(vp);
+        tabLayout.getTabAt(0).setIcon(R.drawable.camera2);
+
+    }
 }
